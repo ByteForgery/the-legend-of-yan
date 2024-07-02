5b108f014172f0b1d87dab2b31279f9ed283f321
Wolfsgesicht00 <yannik.duplitzer@gmail.com>
2024-07-02 11:13:49 +0200

    CameraFollow + Chest-texture

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, -10f);
+    }
+}
